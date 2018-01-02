5ea9eef6f74348465375a347e4e0a6ae96c4e513
Zhexuan Liu <eric@kiwiinc.net>
2018-01-02 15:55:56 +0800

    [MOD] updated host url

--- remote.py
+++ remote.py
@@ -2,7 +2,7 @@
 from celery import Celery
 
 app = Celery('tasks', backend='amqp',
-broker='amqp:///root:Chuangyizhi2013,@139.162.105.73')
+broker='amqp:///root:Chuangyizhi2013,@139.162.118.6')
 
 @app.task
 def add(x, y):
