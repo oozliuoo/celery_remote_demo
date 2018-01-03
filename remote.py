6e8ac95c80c03b6e65e4af4c22757eda08153149
Zhexuan Liu <eric@kiwiinc.net>
2018-01-03 15:24:04 +0800

    [MOD] test redis

--- remote.py
+++ remote.py
@@ -2,7 +2,7 @@
 from celery import Celery
 
 app = Celery('tasks', backend='amqp',
-broker='amqp://root:Chuangyizhi2013,@139.162.118.6/test')
+broker='amqp://139.162.118.6:6379/0')
 
 @app.task(bind=True, track_started=True)
 def add(self, x, y):
